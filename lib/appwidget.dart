902d53c0e26b0637667a7da6fe9fadc2c2a58638
lucasfelr <32622994+lucasfelr@users.noreply.github.com>
2021-11-03 15:31:03 -0300

    Initial commit

---
+++ lib/appwidget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:ola_mundo/webview.dart';
+import 'appcontroller.dart';
+import 'nfcpage.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: AppController.instance,
+      builder: (context, child) {
+        return MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.red,
+            brightness: AppController.instance.isDartTheme
+                ? Brightness.dark
+                : Brightness.light,
+          ),
+          home: WebViewExample(),
+        );
+      },
+    );
+  }
+}
